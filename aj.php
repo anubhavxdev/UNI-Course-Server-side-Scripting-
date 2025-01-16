491ae161978c9336c22584e69f2ae06a1818a724
Anubhav Jaiswal <anubhavjiaswal1803@gmail.com>
2025-01-16 11:56:24 +0530

    Enhance aj.php with additional output statements and type casting examples

--- aj.php
+++ aj.php
@@ -116,8 +116,158 @@
         "PHP",
         "JAVA",
     );
-
     echo c[1];
-?>
+    echo "<br>";
+
+
+
+    echo "The current line number is".__LINE__.".";
+    echo"<br>";
+    echo "Current PHP script name is".__FILE__.".";
+    echo "<br>";
+
+    echo "Directory of the Current PHP script is".__DIR__.".";
+    echo "<br>";
+
+
+    function hello(){
+        $a="Hello World!";
+        echo "$a. The function name is ".__FUNCTION__.".";
+    }
+    hello();
+
+
+
+    $a = 5;
+    $b = 5.35;
+    $c = "Hello";
+    $d = true;
+    $e = NULL;
+
+    
+    $a = (string) $a;
+    $b = (string) $b;
+    $c = (string) $c;
+    $d = (string) $d;
+    $e = (string) $e;
+
+
+    var_dump($a);
+    echo "<br>";
+    var_dump($b);
+    echo "<br>";
+    var_dump($c);
+    echo "<br>";
+    var_dump($d);
+    echo "<br>";
+    var_dump($e);
+    echo "<br>";
+
+
+
+    var_dump(40);
+    echo "<br>";
+    var_dump("aman");
+    echo "<br>";
+    var_dump("2.22");
+    echo "<br>";
+    var_dump(false);
+    echo "<br>";
+    var_dump([21,13,16]);
+    echo "<br>";
+    var_dump(NULL);
+    echo "<br>";
+
+    $x = $y = $z = "Anubhav";
+    echo $x;
+    echo $y;
+    echo $z;
+
+    $x = 5985;
+    echo"<br>";
+    var_dump(is_int($x));
+    echo"<br>";
+    var_dump(is_integer($x));
+
+    $y = 59.85;
+    var_dump(is_float($y));
+    echo "<br>";
+
+//TYPE CASTING
+
+    $saiyam = "25 kilometere";
+    $saiyam = (int) $saiyam;
+    echo $saiyam;
+    var_dump(is_int($saiyam));
+    echo"<br>";
+
+
+    $rishi = "kilometere 25";
+    $rishi = (int) $rishi;
+    echo $rishi;
+    echo"<br>";
+
+    $am = "25 kilometere";
+    $am = (FLOAT) $am;
+    echo $am;
+    var_dump(is_int($am));
+    echo"<br>";
+    
+
+
+    $i = 5;
+    $i = (bool) $i;
+    echo $i;    
+    echo "<br>";
+
+    $ii = 5.34;
+    $ii = (bool) $ii;
+    echo $ii;
+    echo "<br>";
+
+    $iii = 0;
+    $ii = (bool) $iii;
+    echo $ii;
+    echo "<br>";
+
+    $iv = -1;
+    echo $iv;
+    echo "<br>";
+
+    $v = 0.1;
+    $v = (bool) $v;
+    echo $v;
+    echo "<br>";
+
+    $vii = "hello";
+    $vii = (bool) $vii;
+    echo $vii;
+    echo "<br>";
+
+    $viii = "";
+    $viii = (bool) $viii;
+    echo $viii;
+    echo "<br>";
+
+    $ix = true;
+    $ix = (bool) $ix;
+    echo $ix;
+    echo "<br>";
+
+    $x = NULL;
+    $x = (bool) $x;
+    echo $x;
+    echo "<br>";
+    echo "<br>";
+
+
+    $string = "10";
+    $int = 5;
+    $result = (int)$string + $int;
+    echo $result;
+
+
+
+ ?>
 </body>
 </html>
